f2ad70d5bed7fe0b6363f7fd9d7bcddca4e371d6
sebaquirooz <sebaaquirooz@gmail.com>
2025-05-26 08:57:39 -0300

    gestionVehiculos

--- parking/src/interfaz/GestionVehiculos.java
+++ parking/src/interfaz/GestionVehiculos.java
@@ -26,21 +26,131 @@ public class GestionVehiculos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        estadoVehiculo = new javax.swing.JTextField();
+        matriculaVehiculo = new javax.swing.JTextField();
+        marcaVehiculo = new javax.swing.JTextField();
+        modeloVehiculo = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        registrarVehiculo = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        listaVehiculo = new javax.swing.JList();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
+        notaVehiculoSeleccionado = new javax.swing.JLabel();
+        matriculaVehiculoSeleccionado = new javax.swing.JLabel();
+        marcaVehiculoSeleccionado = new javax.swing.JLabel();
+        modeloVehiculoSeleccionado = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        estadoVehiculo.setText("Ingrese estado");
+        estadoVehiculo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                estadoVehiculoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(estadoVehiculo);
+        estadoVehiculo.setBounds(20, 170, 170, 22);
+
+        matriculaVehiculo.setText("Ingrese matricula");
+        matriculaVehiculo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                matriculaVehiculoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(matriculaVehiculo);
+        matriculaVehiculo.setBounds(20, 50, 170, 22);
+
+        marcaVehiculo.setText("Ingrese marca");
+        marcaVehiculo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                marcaVehiculoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(marcaVehiculo);
+        marcaVehiculo.setBounds(20, 90, 170, 22);
+
+        modeloVehiculo.setText("Ingrese modelo");
+        getContentPane().add(modeloVehiculo);
+        modeloVehiculo.setBounds(20, 130, 170, 22);
+
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel3.setText("Vehiculo:");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(220, 190, 150, 17);
+
+        registrarVehiculo.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        registrarVehiculo.setText("Registrar");
+        registrarVehiculo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                registrarVehiculoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(registrarVehiculo);
+        registrarVehiculo.setBounds(20, 210, 90, 23);
+
+        jScrollPane1.setToolTipText("");
+
+        listaVehiculo.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(listaVehiculo);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(220, 50, 110, 120);
+
+        jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
+        getContentPane().add(jSeparator1);
+        jSeparator1.setBounds(200, 20, 10, 260);
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel1.setText("Registrar Vehiculo:");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(20, 20, 150, 20);
+
+        notaVehiculoSeleccionado.setText("<Estado>");
+        getContentPane().add(notaVehiculoSeleccionado);
+        notaVehiculoSeleccionado.setBounds(220, 270, 100, 16);
+
+        matriculaVehiculoSeleccionado.setText("<Matricula>");
+        getContentPane().add(matriculaVehiculoSeleccionado);
+        matriculaVehiculoSeleccionado.setBounds(220, 210, 70, 16);
+
+        marcaVehiculoSeleccionado.setText("<Marca>");
+        getContentPane().add(marcaVehiculoSeleccionado);
+        marcaVehiculoSeleccionado.setBounds(300, 210, 70, 16);
+
+        modeloVehiculoSeleccionado.setText("<Modelo>");
+        getContentPane().add(modeloVehiculoSeleccionado);
+        modeloVehiculoSeleccionado.setBounds(220, 240, 70, 16);
+
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel2.setText("Lista de vehiculos:");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(220, 20, 150, 17);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void matriculaVehiculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_matriculaVehiculoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_matriculaVehiculoActionPerformed
+
+    private void marcaVehiculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_marcaVehiculoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_marcaVehiculoActionPerformed
+
+    private void registrarVehiculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registrarVehiculoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_registrarVehiculoActionPerformed
+
+    private void estadoVehiculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_estadoVehiculoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_estadoVehiculoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +188,20 @@ public class GestionVehiculos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField estadoVehiculo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JList listaVehiculo;
+    private javax.swing.JTextField marcaVehiculo;
+    private javax.swing.JLabel marcaVehiculoSeleccionado;
+    private javax.swing.JTextField matriculaVehiculo;
+    private javax.swing.JLabel matriculaVehiculoSeleccionado;
+    private javax.swing.JTextField modeloVehiculo;
+    private javax.swing.JLabel modeloVehiculoSeleccionado;
+    private javax.swing.JLabel notaVehiculoSeleccionado;
+    private javax.swing.JButton registrarVehiculo;
     // End of variables declaration//GEN-END:variables
 }
