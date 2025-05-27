e3069f49f81e4d8991f5e265dd9154ae88928bd5
sebaquirooz <sq323189@fi365.ort.edu.uy>
2025-05-27 15:56:47 -0300

    uahfhahfh

--- parking/src/prueba/Prueba.java
+++ parking/src/prueba/Prueba.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package prueba;
+import dominio.*;
+import interfaz.*;
 
-/**
- *
- * @author seba2
- */
 public class Prueba {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Sistema unSistema = new Sistema();
     }
     
 }
