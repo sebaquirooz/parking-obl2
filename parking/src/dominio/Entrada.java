a818b3fd4ce378852b760b7154e132ad173ab278
sebaquirooz <sebaaquirooz@gmail.com>
2025-05-29 13:56:37 -0300

    Entradas FINALIZADO (creo) a chequear salidas

--- parking/src/dominio/Entrada.java
+++ parking/src/dominio/Entrada.java
@@ -2,64 +2,26 @@ package dominio;
 
 import java.util.Date;
 
-public class Entrada {
+public class Entrada extends Movimiento {
 
-	private Vehiculo vehiculo;
-        
-        private Empleado empleado;
-
-	private Date fecha;
-        
-        private String hora;
-
-	private String nota;
+    private Salida salida;
 
     public Entrada(Vehiculo vehiculo, Empleado empleado, Date fecha, String hora, String nota) {
-        this.vehiculo = vehiculo;
-        this.empleado = empleado;
-        this.fecha = fecha;
-        this.hora = hora;
-        this.nota = nota;
+        super(vehiculo, empleado, fecha, hora, nota);
+        this.salida = null;
     }
 
-    public Vehiculo getVehiculo() {
-        return vehiculo;
+    public Salida getSalida() {
+        return salida;
     }
 
-    public void setVehiculo(Vehiculo vehiculo) {
-        this.vehiculo = vehiculo;
+    public void setSalida(Salida salida) {
+        this.salida = salida;
     }
 
-    public Empleado getEmpleado() {
-        return empleado;
+    @Override
+    public String toString() {
+        return this.getVehiculo().getMatricula() + " " + this.getFecha() + " " + this.getHora();
     }
 
-    public void setEmpleado(Empleado empleado) {
-        this.empleado = empleado;
-    }
-
-    public Date getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-
-    public String getHora() {
-        return hora;
-    }
-
-    public void setHora(String hora) {
-        this.hora = hora;
-    }
-
-    public String getNota() {
-        return nota;
-    }
-
-    public void setNota(String nota) {
-        this.nota = nota;
-    }
-	
 }
