be4a4ba49bedfe03dac75f9384c60db9b46d75bf
sebaquirooz <sebaaquirooz@gmail.com>
2025-06-20 09:37:25 -0300

    PErsistencia

---
+++ parking/src/dominio/Persistencia.java
@@ -0,0 +1,34 @@
+package dominio;
+
+import java.io.*;
+import java.nio.file.*;
+
+public class Persistencia {
+
+    public static void guardarSistema(Sistema sistema) {
+        try (
+            ObjectOutputStream out = new ObjectOutputStream(
+                    Files.newOutputStream(Paths.get("DATOS.ser")))) {
+            out.writeObject(sistema);
+            System.out.println("Sistema guardado correctamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al guardar el sistema.");
+
+        }
+    }
+
+    public static Sistema cargarSistema() {
+        Sistema unSistema = null;
+        try {
+            ObjectInputStream in = new ObjectInputStream(
+                    Files.newInputStream(Paths.get("datos.ser")));
+            unSistema = (Sistema) in.readObject();
+            System.out.println("Sistema cargado.");
+            in.close();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error de recuperación");
+            e.printStackTrace();
+        }
+        return unSistema;
+    }
